72e9078c847b15bd59700785d765381b316972b5
vahyno <vahyno@gmail.com>
2018-04-20 11:52:20 -0700

    created login routes and sessions model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to:'users#index'
 resources :users
-# get 'users/index'
+
+get "/login", to: "sessions#new"
+get "/logout", to: "sessions#destroy"
+post "/sessions", to: "sessions#create"
 
 end
 
@@ -18,3 +21,13 @@ end
 #      PATCH  /users/:id(.:format)      users#update
 #      PUT    /users/:id(.:format)      users#update
 #      DELETE /users/:id(.:format)      users#destroy
+
+
+# sessions GET    /sessions(.:format)          sessions#index
+#              POST   /sessions(.:format)          sessions#create
+#  new_session GET    /sessions/new(.:format)      sessions#new
+# edit_session GET    /sessions/:id/edit(.:format) sessions#edit
+#      session GET    /sessions/:id(.:format)      sessions#show
+#              PATCH  /sessions/:id(.:format)      sessions#update
+#              PUT    /sessions/:id(.:format)      sessions#update
+#              DELETE /sessions/:id(.:format)      sessions#destroy
