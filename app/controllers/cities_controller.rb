8beab99bedc7a656b1fd6f0c0be3489cfea4546b
stacief510 <staciemfraser@gmail.com>
2018-04-21 13:55:48 -0700

    created show html and controllers for show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,6 +7,22 @@ class CitiesController < ApplicationController
     @city = City.new
   end
 
-  def show
+  def create
+  	city = City.new(city_params)
+  	if city.save
+  		redirect_to cities_path(city)
+  	end
   end
+
+  def show
+  	city_id = params[:id]
+  	@city = City.find_by_id(city_id)
+  end
+
+
+private
+def city_params
+	params.require(:city).permit(:city_name, :photo)
+end
+
 end
