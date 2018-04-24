cf13de8ba49c4cf0a617bc4b334affd12fdc07fb
vahyno <vahyno@gmail.com>
2018-04-24 14:30:45 -0700

    updated seed file, post edit form

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,9 @@ p "Deleted: Post.delete_all, User.delete_all, City.delete_all => all records"
 
 user1 = User.create({first_name: "Gad", last_name:"Faisal", email:"foo@bar.com", password:"123", current_city:"San Francisco"})
 user2 = User.create({first_name: "Method", last_name:"Man", email:"wu@tang.com", password:"123", current_city:"NYC"})
+user3 = User.create({first_name: "Dave", last_name:"T", email:"dt@ga.co", password:"123", current_city:"Oakland"})
+user4 = User.create({first_name: "Chuong", last_name:"Dao", email:"cdao@ga.co", password:"123", current_city:"San Francisco"})
+user5 = User.create({first_name: "Michal's Dad", last_name:"V", email:"mydad@ga.co", password:"123", current_city:"Prague"})
 
 city1 = City.create({city_name:"Paris", photo:"https://www.tabularaisa.com/wp-content/uploads/2015/12/eiffel.jpg"})
 city2 = City.create({city_name:"London", photo:"http://www.ionekatours.com/files/images/cache/1327603710_800_600_1327603710.jpg"})
@@ -24,10 +27,14 @@ post1 = user1.cities << city1
 post2 = user1.cities << city3
 post3 = user2.cities << city3
 post4 = user1.cities << city4
-post5 = user2.cities << city2
+post5 = user5.cities << city2
+post6 = user3.cities << city3
+post7 = user3.cities << city4
+post8 = user4.cities << city2
+
 
 p1 = Post.first
-p1.update(review_title: "Fancy-Pants Paris", review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+p1.update(review_title: "Fancy-Pants Paris", review: "They eat Froggsssss!!!!! And snails!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
 p2 = Post.second
 p2.update(review_title: "I <3 Gibraltar", review: "ostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.")
@@ -38,8 +45,18 @@ p3.update(review_title: "Gibraltar is cool.", review: "em ipsum dolor sit amet, 
 p4 = Post.fourth
 p4.update(review_title: "NYC is awesome", review: "dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. U.")
 
-p5 = Post.last
-p5.update(review_title: "Fun in London", review: "sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.")
+p5 = Post.fifth
+p5.update(review_title: "Bad food in London", review: "Lots of Potatoes, my son loves potatoes.consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.")
+
+p6 = Post.third_to_last
+p6.update(review_title: "Fun in the Sun", review: "dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. U.")
+
+p7 = Post.second_to_last
+p7.update(review_title: "Fun in the big apple", review: " Sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.")
+
+p8 = Post.last
+p8.update(review_title: "Met the Queen", review: "dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. U.")
+
 
 p "++++++++++++++++++++++++++++"
 p "+++All users+++++"
