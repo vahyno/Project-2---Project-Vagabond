311750226607f15108607e27d07106ff6c5e3206
vahyno <vahyno@gmail.com>
2018-04-23 18:49:35 -0700

    added default photo to migration, updated user, post show

--- db/migrate/20180420172711_create_users.rb
+++ db/migrate/20180420172711_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :email
       t.string :password_digest
       t.string :current_city
-      t.string :user_photo
+      t.string :user_photo, :default => "https://www.freeiconspng.com/uploads/am-a-19-year-old-multimedia-artist-student-from-manila--21.png"
       t.binary :user_photo_b
 
       t.timestamps
